3184c5832dd44c3b55aece0e3acbaab3685ec89b
SY Han <elvishan2008@gmail.com>
2022-01-10 23:36:49 +0900

    .junitTest

---
+++ src/main/java/com/toy_01/magicDobby/springboot/web/HelloController.java
@@ -0,0 +1,22 @@
+package com.toy_01.magicDobby.springboot.web;
+
+import com.toy_01.magicDobby.springboot.web.dto.HelloResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
+    }
+}
+
+
