8709757d41d8af27e6615b3674630da65e1dcda0
SY Han <elvishan2008@gmail.com>
2022-01-20 21:48:10 +0900

    naver login implements

--- src/main/java/com/toy_01/magicDobby/springboot/web/IndexController.java
+++ src/main/java/com/toy_01/magicDobby/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.toy_01.magicDobby.springboot.web;
 
+import com.toy_01.magicDobby.springboot.config.auth.LoginUser;
 import com.toy_01.magicDobby.springboot.config.auth.dto.SessionUser;
 import com.toy_01.magicDobby.springboot.service.posts.PostsService;
 import com.toy_01.magicDobby.springboot.web.dto.PostsResponseDto;
@@ -19,11 +20,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
 
         model.addAttribute("posts", postsService.findAllDesc());
-
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null){
             model.addAttribute("userName", user.getName());
